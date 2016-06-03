2fbe7aa3321ab31c94bea02569e4c599cb1a6101
aagarw33 <akashagarawal2@gmail.com>
2016-06-03 15:06:42 -0400

    Some more dance because of re-arch

--- src/main/java/com/akash/spring/crm/webservices/soap/endpoints/CallEndpoint.java
+++ src/main/java/com/akash/spring/crm/webservices/soap/endpoints/CallEndpoint.java
@@ -8,7 +8,9 @@ import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import com.akash.spring.crm.model.Call;
 import com.akash.spring.crm.services.calls.CallService;
 import com.akash.spring.crm.webservices.soap.xml.calls.CallXML;
-import com.akash.spring.crm.webservices.soap.xml.calls.CreateCall;
+import com.akash.spring.crm.webservices.soap.xml.calls.CreateCallRequest;
+import com.akash.spring.crm.webservices.soap.xml.calls.RemoveCallRequest;
+import com.akash.spring.crm.webservices.soap.xml.calls.UpdateCallRequest;
 
 @Endpoint
 public class CallEndpoint {
@@ -18,13 +20,33 @@ public class CallEndpoint {
     @Autowired
     private CallService callService;
     
-    @PayloadRoot(namespace = NAMESPACE, localPart = "createCall")
-    public void createCall(@RequestPayload CreateCall request) {
+    @PayloadRoot(namespace = NAMESPACE, localPart = "createCallRequest")
+    public void createCall(@RequestPayload CreateCallRequest request) {
     	CallXML callXML = request.getCall();
     	Call call = new Call();
     	call.setId(callXML.getId());
     	call.setCallNotes(callXML.getCallNotes());
     	call.setCallTime(callXML.getCallTime().toGregorianCalendar().toZonedDateTime().toLocalDateTime());
     	callService.create(call);
+    }
+    
+    @PayloadRoot(namespace = NAMESPACE, localPart = "removeCallRequest")
+    public void removeCall(@RequestPayload RemoveCallRequest request) {
+    	CallXML callXML = request.getCall();
+    	Call call = new Call();
+    	call.setId(callXML.getId());
+    	call.setCallNotes(callXML.getCallNotes());
+    	call.setCallTime(callXML.getCallTime().toGregorianCalendar().toZonedDateTime().toLocalDateTime());
+    	callService.remove(call);
+    }
+    
+    @PayloadRoot(namespace = NAMESPACE, localPart = "updateCallRequest")
+    public void updateCall(@RequestPayload UpdateCallRequest request) {
+    	CallXML callXML = request.getCall();
+    	Call call = new Call();
+    	call.setId(callXML.getId());
+    	call.setCallNotes(callXML.getCallNotes());
+    	call.setCallTime(callXML.getCallTime().toGregorianCalendar().toZonedDateTime().toLocalDateTime());
+    	callService.update(call);
     }
 }
